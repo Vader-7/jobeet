c1a51b39f5899a860a8fcab1881c00716cd29b7e
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-06 17:26:18 -0300

    feat: update index route

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -8,17 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Job;
 
-/**
- * @Route("job")
- */
+
 class JobController extends AbstractController
 {
     /**
      * Lists all job entities.
      *
      * @Route("/", name="job.list")
-     *
-     * @return Response
      */
     public function list(ManagerRegistry $doctrine) : Response
     {
@@ -28,14 +24,11 @@ class JobController extends AbstractController
             'jobs' => $jobs,
         ]);
     }
-     /**
+    
+    /**
      * Finds and displays a job entity.
      *
-     * @Route("/{id}", name="job.show")
-     *
-     * @param Job $job
-     *
-     * @return Response
+     * @Route("job/{id}", name="job.show")
      */
     public function show(Job $job) : Response
     {
