72f3fb8facc3cc327fd39d3baa3058e4af1ea44f
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-08 15:39:49 -0300

    added Forms

---
+++ src/Form/JobType.php
@@ -0,0 +1,24 @@
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+}
